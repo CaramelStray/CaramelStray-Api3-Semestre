aff8a35c525ae58b49f4e753acca7b980d05f061
William Honda <58149665+Lunix800@users.noreply.github.com>
2025-09-08 18:10:42 -0300

    Ajustes github

--- SpringBootUpAndRunning-chapter3end/java/numero_primo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.numero_primo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.numero_primo to javafx.fxml;
-    exports com.example.numero_primo;
-}
