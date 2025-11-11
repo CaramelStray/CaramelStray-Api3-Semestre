f5f05fa240722df2f479b6c9e64e19d2f3a4ccbe
Patricia Viegas <patriciamvs.cursos@gmail.com>
2025-11-11 20:01:55 -0300

    [feat] adicionado novas classes para o dashboard

---
+++ CaramelStray/src/main/java/br/com/AllTallent/controller/DashboardController.java
@@ -0,0 +1,24 @@
+package br.com.AllTallent.controller;
+
+import br.com.AllTallent.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/dashboard")
+
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping
+    public Map<String, Object> getResumoDashboard() {
+        return dashboardService.gerarResumo();
+    }
+
+}
